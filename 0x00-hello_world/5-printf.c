0ba3a515227f14a8d502713ab3b5461cb0c2f71a
Joseph Precious <josephprecious419@gmail.com>
2023-07-16 08:41:47 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a new line Use the function printf
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return: (0);
+}
+
