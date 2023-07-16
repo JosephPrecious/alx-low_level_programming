45ec77157017bf1ca3df54d144d0a02679ef3ea2
Joseph Precious <josephprecious419@gmail.com>
2023-07-16 08:08:13 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program tha prints a line wih puts funtion and ends with the value 0
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return:(0);
+}
